80b8506bd87e062edff43ac049e88a647afe5cbf
RoberiXPro <andry.robert@protonmail.com>
2025-04-23 18:58:33 +0300

    Update script.js modif svg

--- script.js
+++ script.js
@@ -945,11 +945,11 @@ function updateSeenStatus(msgDiv, data, key) {
     seenCheck.className = "seen-check";
 
     // ✅ Petite icône SVG (WhatsApp-style)
-    seenCheck.innerHTML = `
-      <svg viewBox="0 0 24 24">
-        <path d="M1 13l4 4L23 3M10 14l4 4" stroke="#00ff88" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    `;
+seenCheck.innerHTML = `
+  <svg viewBox="0 0 24 24">
+    <path d="M1 13l4 4L23 3M10 14l4 4" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+`;
 
     msgDiv.appendChild(seenCheck);
   }
